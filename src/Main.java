8bff62067a79451b8e7e2b5f31e2ddb983b743de
BrehaPaula <126786862+BrehaPaula@users.noreply.github.com>
2023-04-03 10:45:11 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Polynomial polynomial1 = new Polynomial("2");
-        Polynomial polynomial2 = new Polynomial("3");
-        Operations.addition(  polynomial1,   polynomial2);
-        Operations.substract(  polynomial1,  polynomial2);
-        Operations.multiply(  polynomial1,  polynomial2);
-    }
-}
--- src/Model.java
+++
@@ -1,5 +0,0 @@
-public interface Model {
-    Polynomial addition(Polynomial p1, Polynomial p2);
-    Polynomial subtract(Polynomial p1, Polynomial p2);
-    Polynomial multiply(Polynomial p1, Polynomial p2);
-}
